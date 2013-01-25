44cd12aee9feb7485ae694712274edcd9941c0d4
Jonathan Filippini <filippini.jo@gmail.com>
2013-01-25 12:20:16 +0100

    few changes more

--- Drag_DropExample/gen/com/example/drag_dropexample/R.java
+++ Drag_DropExample/gen/com/example/drag_dropexample/R.java
@@ -8,24 +8,57 @@
 package com.example.drag_dropexample;
 
 public final class R {
+    public static final class array {
+        public static final int description=0x7f040000;
+    }
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int clear_row=0x7f020000;
+        public static final int empty_row=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int ic_menu_camera_holo_dark=0x7f020003;
+        public static final int ic_popup_reminder=0x7f020004;
+        public static final int redstar=0x7f020005;
+        public static final int start_drag=0x7f020006;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int LinearLayout1=0x7f080000;
+        public static final int gridLeft=0x7f080001;
+        public static final int menu_settings=0x7f080017;
+        public static final int myimage1=0x7f080003;
+        public static final int myimage10=0x7f08000c;
+        public static final int myimage11=0x7f08000d;
+        public static final int myimage12=0x7f08000e;
+        public static final int myimage13=0x7f08000f;
+        public static final int myimage14=0x7f080010;
+        public static final int myimage15=0x7f080011;
+        public static final int myimage16=0x7f080012;
+        public static final int myimage17=0x7f080013;
+        public static final int myimage18=0x7f080014;
+        public static final int myimage19=0x7f080015;
+        public static final int myimage2=0x7f080004;
+        public static final int myimage20=0x7f080016;
+        public static final int myimage3=0x7f080005;
+        public static final int myimage4=0x7f080006;
+        public static final int myimage5=0x7f080007;
+        public static final int myimage6=0x7f080008;
+        public static final int myimage7=0x7f080009;
+        public static final int myimage8=0x7f08000a;
+        public static final int myimage9=0x7f08000b;
+        public static final int scrollView1=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_main=0x7f070000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
+        public static final int app_name=0x7f050000;
+        public static final int desc=0x7f050003;
+        public static final int hello_world=0x7f050001;
+        public static final int menu_settings=0x7f050002;
     }
     public static final class style {
         /** 
@@ -49,10 +82,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
--- Drag_DropExample/src/com/example/drag_dropexample/MainActivity.java
+++ Drag_DropExample/src/com/example/drag_dropexample/MainActivity.java
@@ -1,22 +1,186 @@
 package com.example.drag_dropexample;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 import android.app.Activity;
+import android.content.ClipData;
+import android.content.ClipDescription;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.DragEvent;
 import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.DragShadowBuilder;
+import android.view.View.OnDragListener;
+import android.view.View.OnTouchListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	public final String 			TAG = "element";	
+	private String[]				description;
+	private GridLayout				gridLeft;
+	private TextView[]				texts;
+	private ArrayList<LinearLayout>	layouts;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		description = getResources().getStringArray(R.array.description);
+		
+		ImageView img1 = (ImageView)findViewById(R.id.myimage1);
+		img1.setTag(description[0]);
+		img1.setOnTouchListener(new ElementTouchListener());
+		ImageView img2 = (ImageView)findViewById(R.id.myimage2);
+		img2.setTag(description[1]);
+		img2.setOnTouchListener(new ElementTouchListener());
+		ImageView img3 = (ImageView)findViewById(R.id.myimage3);
+		img3.setTag(description[2]);
+		img3.setOnTouchListener(new ElementTouchListener());
+		
+		findViewById(R.id.myimage3).setOnTouchListener(new ElementTouchListener());
+		findViewById(R.id.myimage4).setOnTouchListener(new ElementTouchListener());
+		findViewById(R.id.myimage5).setOnTouchListener(new ElementTouchListener());
+		findViewById(R.id.myimage6).setOnTouchListener(new ElementTouchListener());
+		findViewById(R.id.myimage7).setOnTouchListener(new ElementTouchListener());
+		findViewById(R.id.myimage8).setOnTouchListener(new ElementTouchListener());
+		findViewById(R.id.myimage9).setOnTouchListener(new ElementTouchListener());
+		findViewById(R.id.myimage10).setOnTouchListener(new ElementTouchListener());
+		findViewById(R.id.myimage11).setOnTouchListener(new ElementTouchListener());
+		findViewById(R.id.myimage12).setOnTouchListener(new ElementTouchListener());
+		findViewById(R.id.myimage13).setOnTouchListener(new ElementTouchListener());
+		findViewById(R.id.myimage14).setOnTouchListener(new ElementTouchListener());
+		findViewById(R.id.myimage15).setOnTouchListener(new ElementTouchListener());
+		findViewById(R.id.myimage16).setOnTouchListener(new ElementTouchListener());
+		findViewById(R.id.myimage17).setOnTouchListener(new ElementTouchListener());
+		findViewById(R.id.myimage18).setOnTouchListener(new ElementTouchListener());
+		findViewById(R.id.myimage19).setOnTouchListener(new ElementTouchListener());
+		findViewById(R.id.myimage20).setOnTouchListener(new ElementTouchListener());
+		
+		gridLeft = (GridLayout)findViewById(R.id.gridLeft);
+			
+		texts	= new TextView[10];
+		layouts = new ArrayList<LinearLayout>();
+		
+		texts[0] = new TextView(this);
+		texts[0].setText("prima riga");
+		
+		texts[1] = new TextView(this);
+		texts[1].setText("seconda riga");
+		
+		texts[2] = new TextView(this);
+		texts[2].setText("terza riga");
+		
+		LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+																	LinearLayout.LayoutParams.MATCH_PARENT);
+		lp.bottomMargin = 20;
+		   
+		LinearLayout l = new LinearLayout(this);
+		l.addView(texts[0], lp);
+		l.addView(new Button(this), lp);
+		
+		layouts.add(l);
+		
+		l = new LinearLayout(this);	
+		l.addView(texts[1], lp);
+		l.addView(new Button(this), lp);
+		layouts.add(l);		
+		
+		l = new LinearLayout(this);
+		l.addView(texts[2], lp);
+		l.addView(new Button(this), lp);
+		layouts.add(l);
+		
+		Iterator<LinearLayout> it = layouts.iterator();
+		ViewGroup.LayoutParams vp = new ViewGroup.LayoutParams(android.view.ViewGroup.LayoutParams.MATCH_PARENT, 10);
+		Log.e("NUMERO ELEMENTI", " " + layouts.size());
+		while (it.hasNext()) {
+			LinearLayout element	= it.next();
+			LinearLayout empty_el	= new LinearLayout(this);
+			empty_el.setTag("empty");
+			
+			empty_el.setOnDragListener(new LineDragListener());
+			gridLeft.addView(empty_el);
+			gridLeft.addView(element);
+			
+		}
+		
+
+		
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
 
+	
+	private class ElementTouchListener implements OnTouchListener {
+		@Override
+		public boolean onTouch(View v, MotionEvent event) {
+			switch(event.getAction()){
+			case MotionEvent.ACTION_DOWN:
+				
+				ClipData data = ClipData.newPlainText("CLIP", (CharSequence) v.getTag());
+				
+				DragShadowBuilder shadowBuilder = new DragShadowBuilder(v);
+				v.startDrag(data, shadowBuilder, v, 0);
+				v.setVisibility(View.INVISIBLE);
+				return true;
+			default:
+				return false;
+			}
+		}
+	}
+	
+	private class LineDragListener implements OnDragListener{
+
+		Drawable starDrag = getResources().getDrawable(R.drawable.start_drag);
+		Drawable emptyRow = getResources().getDrawable(R.drawable.empty_row);
+		Drawable clearRow = getResources().getDrawable(R.drawable.clear_row);
+		private String description;
+		@Override
+		public boolean onDrag(View v, DragEvent event) {
+			int action = event.getAction();
+					
+			
+			switch (action) {
+			case DragEvent.ACTION_DRAG_STARTED:
+				//Draggable
+				if(event.getClipDescription().hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)){
+					v.setBackgroundDrawable(starDrag);
+					return true;		
+				} else {	// Not Draggable
+					return false;
+				}
+			case DragEvent.ACTION_DRAG_ENTERED:
+				v.setBackgroundDrawable(emptyRow);
+				break;
+			case DragEvent.ACTION_DROP:
+				//CLIP DATA reachable only in ACTION_DROP
+				int n = event.getClipData().getItemCount();
+				Log.e("DROPPED", "testo annesso " + description + " CLIP ITEM " + n);
+			case DragEvent.ACTION_DRAG_EXITED:
+				v.setBackgroundDrawable(clearRow);
+				break;
+			default:
+				break;
+			}
+			return true;
+		}
+		
+	}
+	
 }
